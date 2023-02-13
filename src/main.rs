b7f62cc4cb5c0c31ac9ed70fd871a9626170e879
xen <selegakamil@gmail.com>
2023-02-13 18:50:21 +0100

    Single line user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input)?;
+
+    println!("{}", user_input);
+
+    Ok(())
 }
