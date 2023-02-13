e19b1722a70ac5d609edabc2397bb92a83d4115c
xen <selegakamil@gmail.com>
2023-02-13 19:21:45 +0100

    Add command parsing and processing from user input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input)?;
+    let mut input = String::new();
 
-    println!("{}", user_input);
+    while shell::process_input(input.clone()) != shell::ExitCode::Exit {
+        input.clear();
+        io::stdin().read_line(&mut input)?;
+    }
 
     Ok(())
+}
+
+mod shell {
+    #[derive(PartialEq)]
+    pub enum ExitCode {
+        Success,
+        Exit,
+    }
+
+    pub enum Command {
+        None,
+        Exit,
+    }
+
+    pub fn process_input(input: String) -> ExitCode {
+        let command = parse_input(input);
+
+        match command {
+            Command::Exit => ExitCode::Exit,
+            _ => ExitCode::Success,
+        }
+    }
+
+    fn parse_input(input: String) -> Command {
+        let command = input.trim().to_lowercase();
+
+        match command.as_str() {
+            "exit" => Command::Exit,
+            _ => Command::None,
+        }
+    }
 }
